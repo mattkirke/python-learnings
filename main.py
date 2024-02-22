ec7ebc7686b68ca9f101481159a365842d153da2
mattkirke <matthewkirke@icloud.com>
2024-02-22 17:53:02 +0000

    bill calculation with functions

--- main.py
+++ main.py
@@ -31,16 +31,41 @@
 # >70 C 
 # >60 D 
 # <60 F 
-grade = int(input("What is your mark?"))
-if grade >= 90:
-    print("A")
-elif grade >= 80 and grade < 90:
-    print("B")
-elif grade >= 70 and grade < 80:
-    print("C")
-elif grade >= 60 and grade < 70:
-    print("D")
-elif grade > 60:
-    print("F")
-else:
-    print("LOL")
+# grade = int(input("What is your mark?"))
+# if grade >= 90:
+#     print("A")
+# elif grade >= 80 and grade < 90:
+#     print("B")
+# elif grade >= 70 and grade < 80:
+#     print("C")
+# elif grade >= 60 and grade < 70:
+#     print("D")
+# elif grade > 60:
+#     print("F")
+# else:
+#     print("LOL")
+
+ 
+#Multiple arguments, named arguments
+# def greetings( name, greet='hello'):
+#     print(f'{greet} {name}')
+# greetings('Matt', 'hi')
+# greetings(name='Matt', greet='bye')
+
+
+# String formatting 
+# food_amount = float(input("Enter your food amaount: £"))
+# tip_percentage = float(input("Enter your tip percentage: "))/100
+# tip_amount = food_amount * tip_percentage
+
+# total = food_amount + tip_amount
+def billPrice(food_amount, tip_percentage):
+
+    tip_amount = food_amount * tip_percentage
+    total = food_amount + tip_amount
+    print(total)
+    
+food_amount = float(input("Enter your food amaount: £"))
+tip_percentage = float(input("Enter your tip percentage: "))/100
+    
+billPrice(food_amount, tip_percentage)
